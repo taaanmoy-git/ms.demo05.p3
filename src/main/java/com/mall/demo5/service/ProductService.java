ae563a9fe10c2749702b269c62a5c7a5199a6d9a
Tanmoy Saha <taaanmoy.saha@gmail.com>
2025-03-09 02:15:06 +0530

    Added Service

--- src/main/java/com/mall/demo5/service/ProductService.java
+++ src/main/java/com/mall/demo5/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.mall.demo5.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,34 @@ public class ProductService {
 	}
 	
 	public ResponseEntity<List<ProductDTO>> getAllProducts() {
-		
-		return null;	
+		List<Product> product = productRepository.findAll();
+		List<ProductDTO> productDTO = product.stream()
+						.map(ProductDTO::createDTO)
+						.collect(Collectors.toList());
+		return new ResponseEntity<List<ProductDTO>>(productDTO, HttpStatus.OK);	
 	}
+	
+	public ResponseEntity<ProductDTO> getProductById(Long id) {
+		Product product = productRepository.findById(id)
+				.orElseThrow(()->new RuntimeException("Product not found"));
+		return ResponseEntity.ok(ProductDTO.createDTO(product));	
+	}
+	
+	public ResponseEntity<ProductDTO> updateProductById(Long id, ProductDTO productDTO) {
+		Product product = productRepository.findById(id)
+				.orElseThrow(()-> new RuntimeException("Product not found"));
+		product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setQuantity(productDTO.getQuantity());
+        product.setUpdatedAt(productDTO.getUpdatedAt());
+        Product updatedProduct = productRepository.save(product);
+		return new ResponseEntity<>(ProductDTO.createDTO(updatedProduct),HttpStatus.OK);
+	}
+	
+	public ResponseEntity<String> deleteProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        productRepository.delete(product);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
 }
